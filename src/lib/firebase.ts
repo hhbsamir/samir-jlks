dc89383fb3c4d302cc4908964f9554313bacd2ba
SAMIR K. MOHAPATRA <hhbsamir@gmail.com>
2025-08-08 10:13:02 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "jlks-paradip",
+  "appId": "1:168780164501:web:0d528092ea0ff558609a9c",
+  "storageBucket": "jlks-paradip.firebasestorage.app",
+  "apiKey": "AIzaSyBZulAyAzOJVySzxVzpAftT3xi4codPlhM",
+  "authDomain": "jlks-paradip.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "168780164501"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
