7fd7174768f37fe3c70cbc9343383595c05447af
SAMIR K. MOHAPATRA <hhbsamir@gmail.com>
2025-08-08 11:58:22 +0000

    ok

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
         headline: ['Playfair Display', 'serif'],
         code: ['monospace'],
       },
